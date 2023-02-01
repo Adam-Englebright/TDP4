1406b0214c057acb746ca75f07ddecd6765f8c5e
Adam-Englebright <adamenglebright@rocketmail.com>
2023-02-01 00:16:42 +0000

    Added methods to Stepper class to allow stepping by a fixed amount. Needs testing.

--- lib/Stepper/Stepper.cpp
+++ lib/Stepper/Stepper.cpp
@@ -5,9 +5,30 @@
 
 #include "Stepper.h"
 
+
+
+// Global variables to store PWM slice numbers so that they can be used in the interrupt handler bellow.
+uint slice_num_global;
+uint slice_num_counter_global;
+
+
+// Interrupt handler for use by Stepper class.
+void on_pwm_wrap(void)
+{
+    // Disable step PWM output and counter PWM input.
+    pwm_set_enabled(slice_num_global, false);
+    pwm_set_enabled(slice_num_counter_global, false);
+    // Ensure counter is reset to 0 in the counter PWM.
+    pwm_set_counter(slice_num_counter_global, 0);
+    // Clear interrupt flag.
+    pwm_clear_irq(slice_num_counter_global);
+}
+
+
 // Constructor will take stepper frequency, and gpio ID numbers that are used to interface with the driver.
 // These will be used to initalise the appropriate pins as digital and PWM outputs, for control of the driver.
-Stepper::Stepper(uint step_freq, uint enable_port, uint reset_port, uint sleep_port, uint step_port, uint direction_port, uint ms1_port, uint ms2_port, uint ms3_port)
+Stepper::Stepper(uint step_freq, uint enable_port, uint reset_port, uint sleep_port, uint step_port, 
+                 uint direction_port, uint ms1_port, uint ms2_port, uint ms3_port, uint counter_port)
     // Member initalization list (excluding "slice_num", which will be assigned in the body)
     : enable{ enable_port }
     , sleep{ sleep_port }
@@ -17,6 +38,7 @@ Stepper::Stepper(uint step_freq, uint enable_port, uint reset_port, uint sleep_p
     , ms1{ ms1_port }
     , ms2{ ms2_port }
     , ms3{ ms3_port }
+    , counter{ counter_port }
 {
     // --------------- Step PWM ---------------
     // Set up step port as PWM output with specified frequency, initially off.
@@ -26,6 +48,9 @@ Stepper::Stepper(uint step_freq, uint enable_port, uint reset_port, uint sleep_p
 
     // Figure out which slice we just connected to the step pin. Assign this value to "slice_num" for later use.
     slice_num = pwm_gpio_to_slice_num(step_port);
+
+    // Assign slice number to global variable so it can be used in the interrupt handler.
+    slice_num_global = slice_num;
 
     // Get some sensible defaults for the slice configuration. By default, the
     // counter is allowed to wrap over its maximum range (0 to 2**16-1)
@@ -46,6 +71,37 @@ Stepper::Stepper(uint step_freq, uint enable_port, uint reset_port, uint sleep_p
     // However, it does mean we don't need to find what channel our output pin coresponds to.
     // Set this to achieve a 50% duty cycle (using a wrap value of 65535 (the default)).
     pwm_set_both_levels(slice_num, 32768, 32768);
+
+
+    // --------------- PWM Counter ---------------
+    // Set up port as PWM input to count the pulses from the above PWM output.
+    // This will be used to trigger an interrupt to stop the PWM output once a certain number of pulses are counted,
+    // when using the forward_by() and backward_by() methods.
+
+    // Make sure counter pin is on channel B of PWM slice and not on the same slice as the step PWM.
+    assert(pwm_gpio_to_channel(counter_port) == PWM_CHAN_B);
+    slice_num_counter = pwm_gpio_to_slice_num(counter_port);
+    assert(slice_num_counter != slice_num);
+
+    // Assign slice number to global variable so it can be used in the interrupt handler.
+    slice_num_counter_global = slice_num_counter;
+
+    // Get default slice configuration.
+    pwm_config counter_config = pwm_get_default_config();
+
+    // Set up slice to take rising edge inputs on channel B (from the step PWM output) to increment internal counter.
+    pwm_config_set_clkdiv_mode(&counter_config, PWM_DIV_B_RISING);
+
+    // Set up interrupt for when the internal counter wraps
+    pwm_clear_irq(slice_num_counter);
+    pwm_set_irq_enabled(slice_num_counter, true);
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, on_pwm_wrap);
+    irq_set_enabled(PWM_IRQ_WRAP, true);
+
+    // Load the configuration into our PWM slice, initialy not running, and set up GPIO pin as PWM.
+    pwm_init(slice_num_counter, &counter_config, false);
+    gpio_set_function(counter_port, GPIO_FUNC_PWM);
+
 
 
     // --------------- Enable Control ---------------
@@ -111,4 +167,24 @@ void Stepper::backward(void)
 void Stepper::stop(void)
 {
     pwm_set_enabled(slice_num, false);
+}
+
+
+// The forward_by() will move actuator forwards by a specified number of steps.
+void Stepper::forward_by(uint steps)
+{
+    gpio_put(dir, 1);
+    pwm_set_wrap(slice_num_counter, steps-1);
+    pwm_set_enabled(slice_num_counter, true);
+    pwm_set_enabled(slice_num, true);
+}
+
+
+// The backward_by() will move actuator backward by a specified number of steps.
+void Stepper::backward_by(uint steps)
+{
+    gpio_put(dir, 0);
+    pwm_set_wrap(slice_num_counter, steps-1);
+    pwm_set_enabled(slice_num_counter, true);
+    pwm_set_enabled(slice_num, true);
 }
--- lib/Stepper/Stepper.h
+++ lib/Stepper/Stepper.h
@@ -16,15 +16,21 @@ class Stepper
     uint ms2;
     uint ms3;
     uint slice_num;
+    uint slice_num_counter;
+    uint counter;
 public:
     // Constructor will take stepper frequency, and gpio ID numbers that are used to interface with the driver.
-    Stepper(uint, uint, uint, uint, uint, uint, uint, uint, uint);
+    Stepper(uint, uint, uint, uint, uint, uint, uint, uint, uint, uint);
     // Method to start moving actuator forwards.
     void forward(void);
     // Method to start moving actuator backwards.
     void backward(void);
     // Method to stop actuator.
     void stop(void);
+    // Method to move actuator forwards by a specified number of steps.
+    void forward_by(uint);
+    // Method to move actuator backwards by a specified number of steps.
+    void backward_by(uint);
 };
 
 #endif
