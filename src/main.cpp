e712a3e814b2fea890b413b5d369917617774ed1
Adam-Englebright <adamenglebright@rocketmail.com>
2023-02-24 01:04:45 +0000

    More testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "pico/stdlib.h"
 #include "Stepper.h"
-#include "Wire.h"
+#include "hardware/i2c.h"
+#include <stdio.h>
 
 #define STEP_FREQ 15
 #define ENABLE_PIN 28
@@ -16,19 +17,23 @@
 #define LED1_PIN 15
 #define LED2_PIN 14
 
+#define GPIO_SDA0 6
+#define GPIO_SCL0 7
+#define GPIO_SDA1 4
+#define GPIO_SCL1 5
+#define SLAVE_ADDR 8
+
 
 // Function to be called when I2C transmission is recieved.
-void receiveEvent(int howMany)
+void i2c0_irq_handler()
 {
-    gpio_put(LED1_PIN,0);
-    gpio_put(LED2_PIN,1);
+    printf("In the i2c IRQ\n");
+    size_t how_many = i2c_get_read_available(i2c0);
     
-    char buf[howMany];
+    uint8_t buf[how_many];
 
     // Read data transmitted.
-    for (int i=0; i<howMany; i++) {
-        buf[i] = Wire.read();
-    }
+    i2c_read_raw_blocking(i2c0, buf, how_many);
 
     // Set LEDs according to data sent.
     if (buf[0] == 0) {
@@ -37,7 +42,7 @@ void receiveEvent(int howMany)
     }
     else if (buf[0] == 1) {
         gpio_put(LED1_PIN,1);
-        gpio_put(LED2_PIN,0);
+        gpio_put(LED2_PIN,1);
     }
     else if (buf[0] == 2) {
         gpio_put(LED1_PIN,1);
@@ -47,31 +52,28 @@ void receiveEvent(int howMany)
         gpio_put(LED1_PIN,0);
         gpio_put(LED2_PIN,0);
     }
+
+    // Clear interrupt.
+    i2c0->hw->clr_stop_det;
 }
 
 // Create callback function that will handle interupts from GPIO button inputs.
 void gpio_callback(uint gpio, uint32_t events)
 {
     if (gpio == 0 && events == GPIO_IRQ_EDGE_RISE) {
-        gpio_put(LED1_PIN,0);
-        gpio_put(LED2_PIN,1);
-        Wire1.beginTransmission(8);
-        Wire1.write((uint8_t)0);
-        Wire1.endTransmission();
+        printf("Pressing button 1\n");
+        const uint8_t data[] = {0};
+        i2c_write_blocking(i2c1, SLAVE_ADDR, data, sizeof(data), false);
     }
     else if (gpio == 1 && events == GPIO_IRQ_EDGE_RISE) {
-        gpio_put(LED1_PIN,1);
-        gpio_put(LED2_PIN,0);
-        Wire1.beginTransmission(8);
-        Wire1.write((uint8_t)1);
-        Wire1.endTransmission();
+        printf("Pressing button 2\n");
+        const uint8_t data[] = {1};
+        i2c_write_blocking(i2c1, SLAVE_ADDR, data, sizeof(data), false);
     }
     else if (gpio == 13 && events == GPIO_IRQ_EDGE_RISE) {
-        gpio_put(LED1_PIN,0);
-        gpio_put(LED2_PIN,0);
-        Wire1.beginTransmission(8);
-        Wire1.write((uint8_t)2);
-        Wire1.endTransmission();
+        printf("Pressing button 3\n");
+        const uint8_t data[] = {2};
+        i2c_write_blocking(i2c1, SLAVE_ADDR, data, sizeof(data), false);
     }
 }
 
@@ -84,6 +86,9 @@ Stepper stepper(STEP_FREQ, ENABLE_PIN, RESET_PIN, SLEEP_PIN, STEP_PIN, DIR_PIN, 
 
 int main(void)
 {  
+    // Set up USB comms for print debugging.
+    stdio_init_all();
+
     // Set up GPIO input on pins 0, 1, and 13 for control buttons.
     gpio_init(0);
     gpio_set_dir(0, GPIO_IN);
@@ -107,11 +112,30 @@ int main(void)
     gpio_put(LED2_PIN, 0);
 
 
-    // Set up I2C.
-    Wire.begin(6, 7, 8);  // SDA, SCL, Address
-    Wire1.begin(4, 5, 9);
+    // Set up I2C0 as slave.
+    i2c_init(i2c0, 100000);
+    i2c_set_slave_mode(i2c0, true, SLAVE_ADDR);
+    gpio_set_function(GPIO_SDA0, GPIO_FUNC_I2C);
+    gpio_set_function(GPIO_SCL0, GPIO_FUNC_I2C);
+    gpio_pull_up(GPIO_SDA0);
+    gpio_pull_up(GPIO_SCL0);
 
-    Wire.onReceive(receiveEvent);
+    // Enable the I2C interrupts we want to process.
+    i2c0->hw->intr_mask = I2C_IC_INTR_MASK_M_STOP_DET_BITS;
+
+    // Set up the interrupt handler to service I2C interrupts.
+    irq_set_exclusive_handler(I2C0_IRQ, i2c0_irq_handler);
+
+    // Enable I2C interrupt.
+    irq_set_enabled(I2C0_IRQ, true);
+
+
+    // set up I2C1 as master.
+    i2c_init(i2c1, 100000);
+    gpio_set_function(GPIO_SDA1, GPIO_FUNC_I2C);
+    gpio_set_function(GPIO_SCL1, GPIO_FUNC_I2C);
+    gpio_pull_up(GPIO_SDA1);
+    gpio_pull_up(GPIO_SCL1);
 
 
     // Set up interupts on the button inputs.
@@ -121,17 +145,21 @@ int main(void)
 
 
     // Loop forever.
+    uint8_t data[] = {0};
     while (true) {
-        Wire1.beginTransmission(8);
-        Wire1.write((uint8_t)0);
-        Wire1.endTransmission();
+        printf("Writing 0\n");
+        data[0] = 0;
+        i2c_write_blocking(i2c1, SLAVE_ADDR, data, sizeof(data), false);
         sleep_ms(1000);
-        Wire1.beginTransmission(8);
-        Wire1.write((uint8_t)1);
-        Wire1.endTransmission();
+
+        printf("Writing 1\n");
+        data[0] = 1;
+        i2c_write_blocking(i2c1, SLAVE_ADDR, data, sizeof(data), false);
         sleep_ms(1000);
-        Wire1.beginTransmission(8);
-        Wire1.write((uint8_t)2);
-        Wire1.endTransmission();
+
+        printf("Writing 2\n");
+        data[0] = 2;
+        i2c_write_blocking(i2c1, SLAVE_ADDR, data, sizeof(data), false);
+        sleep_ms(1000);
     };
 }
